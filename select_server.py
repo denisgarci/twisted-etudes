2627c2cbf8f3696e06e57c72a2cb37a8ba399e43
Denis Garcia <denisgarci@gmail.com>
2013-11-05 15:34:29 -0500

    Moved rqueue and wqueue to the Client class. Stop using encode and decode, breaks Python3 compatibility

--- select_server.py
+++ select_server.py
@@ -5,32 +5,29 @@ Chat server with only select.
 import select, socket
 
 class ChatServer(object):
-    def __init__(self, addr, name = "server"):
-        self.listener = self.create_listener(addr) # listening socket
-        self.name = name
-        self.clients = [] # clients are Person objects (except for listener)
-        self.rooms = set()
+    def __init__(self, addr):
+        self.addr = addr
+        self.listener = None
+        self.clients = set() # set of clients (without the listener)
 
-        self.wqueue = dict() # values:
-        self.rqueue = dict() # values: string not ending with new line except for [-1]
-
-    def create_listener(self, addr):
+    def create_listener(self):
         listener = socket.socket()
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listener.setblocking(0)
-        listener.bind(addr)
+        listener.bind(self.addr)
         listener.listen(5)
         return listener
 
     @property
     def readers(self):
-        return self.clients + [self.listener]
+        return self.clients.union({self.listener})
 
     @property
     def writers(self):
-        return [client for client in self.wqueue if self.wqueue[client]]
+        return [client for client in self.clients if client.wqueue]
 
     def start(self):
+        self.listener = self.create_listener()
         while True:
             self.select_loop()
 
@@ -39,55 +36,49 @@ class ChatServer(object):
 
         for rclient in rlist:
             if rclient is self.listener:
-                # Listener received connection request
-                self.handle_listener()
+                self.handle_listener()  # Listener received connection request
             else:
-                self.get_msg(rclient)
+                self.recv_msg(rclient)
 
         for wclient in wlist:
-            for msg_list in self.rqueue:
-                if msg_list:
-                    msg = msg_list.pop(0)
-                    self.handle_msg(rclient, msg)
+            self.send_msg(wclient)
 
-            next_msg = self.wqueue[wclient].pop(0)
-            amt_sent = wclient.sock.send(next_msg)
-            self.wqueue[wclient].insert(0, next_msg[amt_sent:])
+    def send_msg(self, client):
+        msg = client.wqueue[0]
+        amt_sent = client.sock.send(msg)
+        if amt_sent == len(msg):
+            client.wqueue.pop(0)
+        else:
+            client.wqueue[0] = msg[amt_sent:]
 
-    def get_msg(self, client):
-         # SHOULD WE DECODE HERE? msg.decode()?
-        # handles parsing messages
-        msg = client.sock.recv(10).decode()
+
+    def recv_msg(self, client):
+        # SHOULD WE DECODE HERE? msg.decode()? no if we don't have the whole bytes of a utf8 char
+        msg = client.sock.recv(10)
         if not msg:
             self.clients.remove(client)
         else:
-            print("received", msg)
-            msg = "".join((self.rqueue[client], msg))
-            #if msg.endswith("\n"):
-                #msg_to_send = msg
-            #else:
-                #try:
-                    #msg_to_send = msg[:msg.rindex("\n")+1]
-                #except ValueError:
-                    #self.rqueu[client] = msg
-                    #msg_to_send = None
-            self.handle_msg(client, msg)
+            print('Server received {0} from {1}'.format(msg, client.name))
+            # Check if we actually received an entire message. An entire message is just a message finishing with an
+            msg = ''.join((client.rqueue, msg))
+            if msg.endswith("\n"):
+                self.handle_msg(client, msg)
+            else:
+                self.client.rqueue = msg
 
+    def handle_msg(self, sender, msg):
+        for client in self.clients:
+            if client is not sender:
+                client.wqueue.append(msg)
 
     def handle_listener(self):
         sock, addr = self.listener.accept()
         client = Client(sock, addr)
-        self.clients.append(client)
-        self.wqueue[client] = []
-        print("{} has connected.".format(client.name))
-        # Insert welcome message.
+        self.clients.add(client)
+        print('{} has connected.'.format(client.name))
+        client.wqueue.append('Welcome {0}. This is your address {1}.'.format(client.name, addr))
 
-    def handle_msg(self, sender, msg): # sender = socket
-        print("Client says:", msg)
-        #self.send_msg_to_room(sender, msg)
-        for client in self.clients:
-            if client is not sender:
-                self.wqueue[client].append(msg.encode())
+
 
 
     #def send_msg_to_room(self, sender, msg):
@@ -99,41 +90,26 @@ class ChatServer(object):
 
 
 
-
     def remove(self, client):
         client.sock.close()
         self.clients.remove(client)
 
 
-class Room(object):
-    def __init__(self, name):
-        self.name = name
-        self.clients = set()
-
-    # def add_client(self):
-    #     pass
-
-    def kick_client(self):
-        pass
-
-
 class Client(object):
-    def __init__(self, sock, addr, name="anon"):
+
+    counter = 0
+
+    def __init__(self, sock, addr, name=None):
+        self.client_id = Client.counter
+        Client.counter += 1
         self.sock = sock
         self.addr = addr
-        self.name = name
-        self.room = None
-
-    def set_name(self, name):
+        if name == None:
+            name = 'client{}'.format(self.client_id)
         self.name = name
 
-    def join_room(self, room):
-        room.clients.add(self)
-        self.rooms.add(room)
-
-    def leave_room(self, room):
-        room.clients.remove(self)
-        self.rooms.remove(room)
+        self.rqueue = ''
+        self.wqueue = []
 
     def fileno(self):
         return self.sock.fileno()
